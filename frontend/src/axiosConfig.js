2224a4c195a822f0b6b5fb602a8e26d77bab98a6
Sagar <sagarjha2004@gmail.com>
2025-04-21 03:45:30 +0530

    updated backend url

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
+// Automatically attach token if exists
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers['auth-token'] = token;
+  if (token) {
+    config.headers['auth-token'] = token;
+  }
   return config;
 });
 
