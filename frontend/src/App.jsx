c398fa18a2e0eaa962ffe634fc4745f14931da93
Sagar <sagarjha2004@gmail.com>
2025-04-21 21:33:55 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Upload from "./pages/Upload";
@@ -14,19 +13,8 @@ import './index.css';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        <h1>Welcome to 🛒 MarketPlace</h1>
-        <div style={{ display: 'flex', justifyContent: 'center', gap: '1rem', flexWrap: 'wrap', marginTop: '2rem' }}>
-          <Link to="/upload"><button className="homepage-btn">Upload Product</button></Link>
-          <Link to="/purchases"><button className="homepage-btn">My Purchases</button></Link>
-          <Link to="/login"><button className="homepage-btn">Login</button></Link>
-          <Link to="/sells"><button className="homepage-btn">My Sells</button></Link>
-          <Link to="/register"><button className="homepage-btn">Register</button></Link>
-        </div>
-      </div>
       <Routes>
-        <Route path='/' element={<Marketplace />} />
+        <Route path='/' element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/upload' element={<Upload />} />
@@ -34,6 +22,7 @@ function App() {
         <Route path='/sells' element={<Sells />} />
         <Route path='/all-products' element={<AllProductsDashboard />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/marketplace' element={<Marketplace />} />
       </Routes>
     </Router>
   );
