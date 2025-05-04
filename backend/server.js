b02fd54ece975fb49ae89becd8f4844a9234374e
Sagar <sagarjha2004@gmail.com>
2025-05-04 13:39:26 +0530

    updated server.js for login error

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
 
 // CORS setup
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://student-helper-yaye.vercel.app'],
   credentials: true
 }));
 
