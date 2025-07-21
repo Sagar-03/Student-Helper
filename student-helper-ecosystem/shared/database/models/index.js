71992a16d9bdad96b0b72158c1834e4187190801
Sagar <sagarjha2004@gmail.com>
2025-07-21 17:00:17 +0530

    remove severlity

--- student-helper-ecosystem/shared/database/models/index.js
+++
@@ -1,8 +0,0 @@
-// filepath: student-helper-ecosystem/shared/database/models/index.js
-const Product = require('./Product');
-const User = require('./User');
-
-module.exports = {
-  Product,
-  User
-};
